68b433215e149804d3706989a0705d84243354de
simone.guidi2 <simone.guidi@quadrata-group.com>
2026-06-01 16:08:49 +0200

    aggiunti item al template ed allo script

--- rm4mini-temphum.py
+++ rm4mini-temphum.py
@@ -3,18 +3,31 @@ import broadlink
 import sys
 import json
 
-DEVICE_IP = sys.argv[1]
-DEVICE_PORT = 80
-DEVICE_TYPE = 0x653c
-DEVICE_MAC = sys.argv[2]
 
-device = broadlink.gendevice(DEVICE_TYPE, (DEVICE_IP, DEVICE_PORT), DEVICE_MAC)
+def normalize_mac(value):
+    return ''.join(c for c in value.lower() if c in '0123456789abcdef')
+
+DEVICE_IP = sys.argv[1]
+DEVICE_MAC = normalize_mac(sys.argv[2])
+
+device = broadlink.hello(DEVICE_IP)
+if device.mac.hex().lower() != DEVICE_MAC:
+    raise RuntimeError('Discovered device MAC does not match configured MAC')
+
 device.auth()
+device.update()
 data = device.check_sensors()
 
 result_json = {
     "Temp": data['temperature'],
-    "Hum": data['humidity']
+    "Hum": data['humidity'],
+    "DeviceName": device.name,
+    "DeviceType": device.get_type(),
+    "Model": device.model,
+    "Manufacturer": device.manufacturer,
+    "ProductId": '0x%04X' % device.devtype,
+    "FirmwareVersion": device.get_fwversion(),
+    "Locked": int(device.is_locked)
 }
 
 print(json.dumps(result_json))
